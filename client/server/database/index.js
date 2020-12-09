84ec0ddf04862907db20d7ba1505b308698a0313
Chris Kabana <chris.kabana@gmail.com>
2020-12-08 18:10:41 -0600

    more setup for passport

--- client/server/database/index.js
+++ client/server/database/index.js
@@ -1,1 +1,23 @@
-// placeholder
+// Connect to Mongo database
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+
+// local database url
+// 27017 is the default mongoDB port
+const uri = "mongodb://localhost:27017/reactreadinglist";
+
+mongoose.connection(uri).then(
+  () => {
+    // ready to use, the 'mongoose.connect()' promise resolves to undefined.
+    console.log("Connected to Mongo");
+
+  },
+  (err) => {
+    // handle initial connection error
+    console.log("error connecting to Mongo: ");
+    console.log(err);
+
+  }
+);
+
+module.exports = mongoose.connection
--- client/server/database/models/user.js
+++ client/server/database/models/user.js
@@ -1,1 +1,38 @@
-// placeholder
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema
+const bcrypt = require('bcryptjs');
+mongoose.promise = Promise
+
+// Define userSchema
+const userSchema = new Schema({
+
+    username: { type: String, unique: false, required: false },
+    password: { type: String, unique: false, required: false }
+
+})
+
+// defining schema methods
+userSchema.methods = {
+    checkPassword: function(inputPassword) {
+        return bcrypt.compareSync(inputPassword, this.password)
+    },
+    hashPassword: plainTextPassword => {
+        return bcrypt.hashSync(plainTextPassword, 10)
+    }
+}
+
+// defining hooks for pre-savings
+userSchema.pre('save', function(next) {
+    if (!this.password) {
+        console.log('models/user.js: NO PASSWORD PROVIDED')
+        next()
+    } else {
+        console.log('models/user.js: hasPassword in pre-save');
+
+        this.password = this.hashPassword(this.password)
+        next()
+    }
+})
+
+const User = mongoose.model('User', userSchema)
+module.exports = User
