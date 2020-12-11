07f440aec2b228fac6e1cb64c81ca6ceb7c3ad5f
Matt Dudzik <mdudzik92@gmail.com>
2020-12-10 22:55:37 -0600

    Working Google auth in Navbar

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -2,6 +2,8 @@ import React, { useState, useRef } from "react";
 import { List, ListItem } from "../List";
 // import { Link } from "react-router-dom";
 import Navbar from "react-bootstrap/Navbar";
+import Login from "../Login";
+import Logout from "../Logout";
 import "bootstrap/dist/css/bootstrap.min.css";
 // import { Container } from "react-bootstrap";
 import "./style.css";
@@ -11,97 +13,55 @@ import NavbarCollapse from "react-bootstrap/esm/NavbarCollapse";
 
 // props.setCurrentPage will refer to the state of the currentPage from App.js
 const Nav = (props) => {
-  const [isNavCollapsed, setIsNavCollapsed] = useState(true);
-  const ref1 = useRef(null)
-  const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
+	const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+	const ref1 = useRef(null);
+	const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
 
-  const handleClick = (e) => {
-    console.log(e.target.id);
-    console.log("fired")
-    props.setCurrentPage(e.target.id)
-  }
+	const handleClick = (e) => {
+		console.log(e.target.id);
+		console.log("fired");
+		props.setCurrentPage(e.target.id);
+	};
 
-  return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light rounded">
-      <a className="navbar-brand text-info font-weight-bolder" href="/">
-        <span className="title">Abode</span>
-      </a>
-      <button
-        className="custom-toggler navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarsExample09"
-        aria-controls="navbarsExample09"
-        aria-expanded={!isNavCollapsed ? true : false}
-        aria-label="Toggle navigation"
-        onClick={handleNavCollapse}
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
+	return (
+		<nav className="navbar navbar-expand-lg navbar-light bg-light rounded">
+			<a className="navbar-brand text-info font-weight-bolder" href="/">
+				<span className="title">Abode</span>
+			</a>
+			<button
+				className="custom-toggler navbar-toggler"
+				type="button"
+				data-toggle="collapse"
+				data-target="#navbarsExample09"
+				aria-controls="navbarsExample09"
+				aria-expanded={!isNavCollapsed ? true : false}
+				aria-label="Toggle navigation"
+				onClick={handleNavCollapse}
+			>
+				<span className="navbar-toggler-icon"></span>
+			</button>
 
-      <div
-        className={`${isNavCollapsed ? "collapse" : ""} navbar-collapse`}
-        id="navbarsExample09"
-      >
-        <div className="description">
-          <p>
-            Abode simiplifies your life by providing a single location to keep track
-            of all of the important things in your life. Track your bills, important events,
-            chores and tasks to be done, and even a list of groceries you need to buy!
-            Abode instills order to a disordered world.
-          </p>
-        </div>
-        <Link
-          className="nav-link text-info"
-          activeClass="active-link"
-          to=""
-          smooth={true}
-          spy={true}
-        >
-          Log In
-        </Link>
+			<div
+				className={`${isNavCollapsed ? "collapse" : ""} navbar-collapse`}
+				id="navbarsExample09"
+			>
+				<div className="description">
+					<p>
+						Abode simiplifies your life by providing a single location to keep
+						track of all of the important things in your life. Track your bills,
+						important events, chores and tasks to be done, and even a list of
+						groceries you need to buy! Abode instills order to a disordered
+						world.
+					</p>
+				</div>
 
-        <Link
-          className="nav-link text-info"
-          activeClass="active-link"
-          to=""
-          smooth={true}
-          spy={true}
-        >
-          Sign Up
-        </Link>
-        
-        <div id="features"
-          className="nav-link text-info"
-          activeClass="active-link"
-          onClick={handleClick}
-          to="#options"
-          smooth={true}
-          spy={true}
-        >
-          Features
-        </div>
-
-        <div id="team"
-          className="nav-link text-info"
-          activeClass="active-link"
-          to="#aboutUs"
-          onClick={handleClick}
-          smooth={true}
-          spy={true}
-          // onClick function change a state value at App.js level
-          // that would keep track of which page is currently rendering
-          // make a useRef ref for each element you want to scroll to
-        >
-          About Us
-        </div>
-        
-        <a href="#" className="btn btn-sm btn-info nav-link text-white">
-          Test3
-        </a>
-      </div>
-    </nav>
-  );
+				<div>
+					<Login />
+					<Logout />
+				</div>
+			</div>
+		</nav>
+	);
 };
 
 export default Nav;
