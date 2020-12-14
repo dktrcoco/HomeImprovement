c53ed3dc5df04af99a64baf95f06f63b2fabb50f
CVade <christian.vadevoulis@gmail.com>
2020-12-14 13:30:37 -0600

    carousel css tweaks

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,11 +3,11 @@ import "../styles/Footer.css";
 
 function Footer() {
     return (
-            <div className="footer">
-                <footer className="card-footer footer-font-style text-muted footer-position">
-                    <p>Helping you organize your Home since 2020</p>
-                </footer>
-            </div>
+        <div className="footer">
+            <footer className="card-footer footer-font-style text-muted footer-position">
+                <h5>Helping you organize your Home since 2020!</h5>
+            </footer>
+        </div>
     );
 }
 
